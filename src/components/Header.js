c86322150884e323bb7a7f74e81c3a45c19bd155
Kindziolek <kingapietron@gmail.com>
2023-01-12 23:38:42 +0100

    Header-nav scss

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,38 +3,30 @@ import { Link } from "react-scroll";
 
 const Header = () => {
   return (
-
-    <div>
-        <div>
-            <Link to="/logowanie">
-                <button>Zaloguj</button>
-            </Link>
-            <Link to="/rejestracja">
-                <button>Załóż konto</button>
-            </Link>
-        </div>
-        <div>
-            <nav>
-                <li>Start</li>
-                <li>O co chodzi?</li>
-                <li>O nas</li>
-                <li>Funadacja i organizacje</li>
-                <li>Kontakt</li>
-            </nav>
-        </div>
-        <div>
-            <h1>Zacznij pomagać! Oddaj niechciane rzeczy w zaufane ręce</h1>
-            <Link to="/logowanie">
-                <button>ODDAJ RZECZY</button>
-            </Link>
-            <Link to="/logowanie">
-                <button>ZORGANIZUJ ZBIÓRKĘ</button>
-            </Link>
-        </div>
+    <div className="header__container">
+      <div className="login__box">
+        <Link to="/logowanie" className="login-btn">Zaloguj</Link>
+        <Link to="/rejestracja" className="login-btn">Załóż konto</Link>
+      </div>
+      <div>
+        <nav className="nav">
+          <li className="nav-item">Start</li>
+          <li className="nav-item">O co chodzi?</li>
+          <li className="nav-item">O nas</li>
+          <li className="nav-item">Funadacja i organizacje</li>
+          <li className="nav-item">Kontakt</li>
+        </nav>
+      </div>
+      <div>
+        <h1>Zacznij pomagać! Oddaj niechciane rzeczy w zaufane ręce</h1>
+        <Link to="/logowanie">
+          <button>ODDAJ RZECZY</button>
+        </Link>
+        <Link to="/logowanie">
+          <button>ZORGANIZUJ ZBIÓRKĘ</button>
+        </Link>
+      </div>
     </div>
-
-
-
   );
 };
 
